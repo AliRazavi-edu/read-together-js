96ef8aca6302c875c24b365704ebbfb346ea17c5
Mohammad Amanalikhani <md.akhi.ir@gmail.com>
2021-03-10 17:43:25 +0330

    feat: init i18n

---
+++ config/i18n.js
@@ -0,0 +1,12 @@
+module.exports = {
+  fa: {
+    default: true,
+    path: `fa`,
+    locale: `fa-IR`,
+    dateFormat: `DD/MM/YYYY`,
+    siteLanguage: `fa`,
+    ogLanguage: `fa-IR`,
+    defaultTitle: `Using i18n with Gatsby`,
+    defaultDescription: `Gatsby example site using MDX and dependency-free i18n`,
+  }
+}
