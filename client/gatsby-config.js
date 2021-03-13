83dab44a0805e7f791bb35743675f06c57ab8ac4
Mohammad Amanalikhani <md.akhi.ir@gmail.com>
2021-03-13 15:36:30 +0330

    chore(package): add info repo

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -1,5 +1,21 @@
 module.exports = {
-    plugins: [
+  siteMetadata: {
+    title: "",
+    author: "",
+    description: "",
+    siteUrl: "",
+    social: [
+      {
+        name: "twitter",
+        url: "",
+      },
+      {
+        name: "github",
+        url: "",
+      },
+    ],
+  },
+  plugins: [
 
     ]
 }
